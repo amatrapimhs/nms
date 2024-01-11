1ed2dbd19e9d265645dc1ac56a68b0a37de9fe36
amatrapimhs <149750479+amatrapimhs@users.noreply.github.com>
2024-01-11 18:42:46 +0700

    Add files via upload

---
+++ uas/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>MASERATI SEMARANG</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<body background="background-batik-putih-2-300x300-1.png">
+  <div class="header">
+    <p>MASERATI SEMARANG</p>
+  </div>
+  <center><img src="logo.png" height="200" width="300"></center>
+  <nav>
+    <ul> <?php include("page/navbar.php") ?> </ul>
+  </nav>
+
+  <?php include("content.php") ?>
+
+  <?php include("page/user/keranjang.php") ?>
+  <div class="footer">
+    <p>copyright by | pace | Repost by <a href="" target="_blank">Framework Id</a></p>
+  </div>
+  <center><img src="logo.png"></center>
+  
+</body>
+
+</html>
